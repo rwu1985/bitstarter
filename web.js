947bc14589ed167fc352569d839ca287c250df22
Ray Wu <rwu1985@gmail.com>
2013-08-04 08:32:19 +0000

    updated web.js with buffer and read

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file_buffer = fs.readFileSync('index.html');
+  var file_string = file_buffer.toString('String');
+
+  response.send(file_string);
 });
 
 var port = process.env.PORT || 5000;
